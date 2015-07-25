e62c6ce2c0ff43d01f2e3cff1f0400d9aa8f9054
Ryan Davis <ryand@zenspider.com>
2015-07-25 12:50:40 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest-focus/dev/": change = 10330]

--- lib/minitest/focus5.rb
+++ lib/minitest/focus5.rb
@@ -1,6 +1,6 @@
 class Minitest::Test    # :nodoc:
   class Focus           # :nodoc:
-    VERSION = "1.1.1"   # :nodoc:
+    VERSION = "1.1.2"   # :nodoc:
   end
 
   @@filtered_names = [] # :nodoc:
