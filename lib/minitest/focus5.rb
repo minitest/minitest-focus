56e91616d222c65c386c7523a48d3e1a00ef9bb6
Ryan Davis <ryand@zenspider.com>
2015-12-10 16:12:24 -0800

    minor refactoring [git-p4: depot-paths = "//src/minitest-focus/dev/": change = 10449]

--- lib/minitest/focus5.rb
+++ lib/minitest/focus5.rb
@@ -5,6 +5,22 @@ class Minitest::Test    # :nodoc:
 
   @@filtered_names = [] # :nodoc:
 
+  def self.add_to_filter name
+    @@filtered_names << "#{self}##{name}"
+    filter = "/^(#{Regexp.union(@@filtered_names).source})$/"
+
+    index = ARGV.index("-n")
+
+    if index then
+      warn "NOTE: Found `-n <regexp>` arg. This breaks under Rake::TestTask"
+    end
+
+    index = ARGV.index { |arg| arg =~ /^-n/ }
+    ARGV.delete_at index if index
+
+    ARGV << "-n=#{filter}"
+  end
+
   ##
   # Focus on the next test defined. Cumulative. Equivalent to
   # running with command line arg: -n /test_name|.../
@@ -20,19 +36,7 @@ class Minitest::Test    # :nodoc:
     meta = class << self; self; end
 
     meta.send :define_method, :method_added do |name|
-      @@filtered_names << "#{self}##{name}"
-      filter = "/^(#{Regexp.union(@@filtered_names).source})$/"
-
-      index = ARGV.index("-n")
-
-      if index then
-        warn "NOTE: Found `-n <regexp>` arg. This breaks under Rake::TestTask"
-      end
-
-      index = ARGV.index { |arg| arg =~ /^-n/ }
-      ARGV.delete_at index if index
-
-      ARGV << "-n=#{filter}"
+      add_to_filter name
 
       meta.send :remove_method, :method_added
     end
